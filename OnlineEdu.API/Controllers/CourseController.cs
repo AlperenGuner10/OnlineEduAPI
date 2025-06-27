984b231e10280e851b2ab284846284797c58c7da
AlperenGuner10 <alperenguner2210@gmail.com>
2025-06-27 20:43:15 +0300

    addControllers

---
+++ OnlineEdu.API/Controllers/CourseController.cs
@@ -0,0 +1,48 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using OnlineEdu.Business.Abstract;
+using OnlineEdu.DTO.DTOs.AboutDTOs;
+using OnlineEdu.DTO.DTOs.CourseDTOs;
+using OnlineEdu.Entity.Entities;
+
+namespace OnlineEdu.API.Controllers
+{
+	[Route("api/[controller]")]
+	[ApiController]
+	public class CourseController(IGenericService<Course> _courseService, IMapper _mapper) : ControllerBase
+	{
+		[HttpGet]
+		public IActionResult Get()
+		{
+			var values = _courseService.TGetList();
+			return Ok(values);
+		}
+		[HttpGet("{id}")]
+		public IActionResult GetById(int id)
+		{
+			var value = _courseService.TGetById(id);
+			return Ok(value);
+		}
+		[HttpDelete("{id}")]
+		public IActionResult Delete(int id)
+		{
+			_courseService.TDelete(id);
+			return Ok("Kurs Alanı Silindi");
+		}
+		[HttpPost]
+		public IActionResult Create(CreateCourseDto createCourseDTO)
+		{
+			var value = _mapper.Map<Course>(createCourseDTO);
+			_courseService.TCreate(value);
+			return Ok("Yeni Kurs Alanı Oluşturuldu.");
+		}
+		[HttpPut]
+		public IActionResult Update(UpdateCourseDto updateCourseDTO)
+		{
+			var value = _mapper.Map<Course>(updateCourseDTO);
+			_courseService.TUpdate(value);
+			return Ok("Kurs Alanı Güncellendi");
+		}
+	}
+}
---
+++ OnlineEdu.API/Mapping/SubscribeMapping.cs
@@ -0,0 +1,15 @@
+﻿using AutoMapper;
+using OnlineEdu.DTO.DTOs.SubscriberDTOs;
+using OnlineEdu.Entity.Entities;
+
+namespace OnlineEdu.API.Mapping
+{
+	public class SubscribeMapping : Profile
+	{
+		public SubscribeMapping()
+		{
+			CreateMap<CreateSubscriberDto, Subscriber>().ReverseMap();
+			CreateMap<UpdateSubscriberDto, Subscriber>().ReverseMap();
+		}
+	}
+}
